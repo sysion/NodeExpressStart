c1f897142afa564945d20629ae25f133bb918aa1
sysionng <sysionng@gmail.com>
2022-05-23 12:32:36 +0100

    Completed backend server.js and frontend index.js basic functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');                 //required for testing - Cross-Origin Resource Sharing
 const sqlite3 = require('sqlite3').verbose();
 const path = require("path");
 const bodyParser = require('body-parser');
@@ -19,6 +19,17 @@ const limiter = rateLimit({
   max: 100 // limit each IP to 100 requests per windowMs
 });
 
+app.use(cors({
+    origin: 'http://127.0.0.1:8080'
+}));
+
+/*app.use(cors({ //ok
+    origin: ['http://127.0.0.1:8080', 'http://127.0.0.1:9238']
+}));
+
+app.use(cors({ //ok
+    origin: '*'
+}));*/
 
 app.get('/api/houses', function(req, res){
   //res.send('hi yaa');   //testing ok
@@ -26,10 +37,35 @@ app.get('/api/houses', function(req, res){
   res.send(db);
 });
 
-/*app.get('/api/house', function(req, res){
+app.get('/api/houses/:id', function(req, res){
   res.type('application/json');
-  res.send(db);
-});*/
+  let house = '';
+  const id = req.params.id;     // get id from the URL
+  db.forEach((hs) => {
+    if (hs['id'] === parseInt(id)){ //id in URL is string hence needs to be converted to integer number
+      house = hs;
+    }
+  });
+  res.send(house);
+});
+
+app.get('/images/:name', function(req, res){
+  res.type('application/blob');
+  const name = req.params.name;     // get name from the URL
+  res.send('http://127.0.0.1:8042/db/images/' + name);
+
+
+  /*/ Searching books for the isbn
+  for (let book of books) {
+    if (book.isbn === isbn) {
+      res.json(book);
+      return;
+    }
+  }
+
+  // Sending 404 when not found something is a good practice
+  res.status(404).send('Book not found');*/
+});
 
 server.listen(port, function(){
   console.log('server running on http://localhost:' + port);
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,74 @@
-const api_url = 'http://127.0.0.1:8042';
-const endpoint = 'api/houses';
+const api_origin = 'http://127.0.0.1:8042';
+const main = document.getElementById('content');
+
+async function getHouses(){
+	try {
+		const endpoint = 'api/houses';
+		let response = await fetch(`${api_origin}/${endpoint}`);
+
+		if (!response.ok) {  //or (response.status === 200)
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+
+		return await response.json();
+	} catch(e) {
+		//console.log('getHouses:', e);
+		console.log(e);
+	}
+}
+
+async function getHouse(id){
+	try {
+		const endpoint = 'api/houses/' + id;
+		let response = await fetch(`${api_origin}/${endpoint}`);
+
+		if (!response.ok) {  //or (response.status === 200)
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+
+		return await response.json();
+	} catch(e) {
+		//console.log('getHouses:', e);
+		console.log(e);
+	}
+}
+
+async function renderViewAll(){
+	try {
+		let houses = await getHouses();
+		let html = '';
+
+		houses.forEach((house) => {
+			let house_html =`<div class='house'> 
+								<h1>${house.address}</h1>
+								<h2>${house.agent} <span>: ${house.code}</span></h2>
+								<img src=${house.url} alt=${house.code} />
+							</div>`;
+
+			html += house_html;
+		});
+
+		main.innerHTML = html;
+	} catch(e) {
+		console.log(e);
+	}
+}
+
+async function renderView(id){
+	try {
+		let house = await getHouses(id);
+		
+		let html =`<div class='house'> 
+						<h1>${house.address}</h1>
+						<h2>${house.agent} <span>: ${house.code}</span></h2>
+						<img src=${house.url} alt=${house.code} />
+					</div>`;
+
+		main.innerHTML = html;
+	} catch(e) {
+		console.log(e);
+	}
+}
+
+
+window.addEventListener('DOMContentLoaded', renderViewAll);
